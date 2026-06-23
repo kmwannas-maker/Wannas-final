fa36a918096217facf1937e3043a64a83ab35550
kmwannas-maker <kmwannas@gmail.com>
2026-06-23 19:59:02 +0300

    Update dark_mode_provider.dart

--- lib/core/providers/dark_mode_provider.dart
+++ lib/core/providers/dark_mode_provider.dart
@@ -1,24 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'shared_prefs_provider.dart';
 
 final darkModeProvider =
-    StateNotifierProvider<DarkModeNotifier, bool>((ref) => DarkModeNotifier());
+    StateNotifierProvider<DarkModeNotifier, bool>((ref) {
+  final prefs = ref.watch(sharedPreferencesProvider);
+  return DarkModeNotifier(prefs);
+});
 
 class DarkModeNotifier extends StateNotifier<bool> {
-  DarkModeNotifier() : super(false) {
-    _load();
-  }
-
+  final SharedPreferences _prefs;
   static const _key = 'dark_mode';
 
-  Future<void> _load() async {
-    final prefs = await SharedPreferences.getInstance();
-    state = prefs.getBool(_key) ?? false;
-  }
+  DarkModeNotifier(this._prefs) : super(_prefs.getBool(_key) ?? false);
 
   Future<void> toggle() async {
     state = !state;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_key, state);
+    await _prefs.setBool(_key, state);
   }
 }
